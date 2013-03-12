cf846911baf94bc453ff56d5d4db8ddd4c8701f9
Greg Mathews <gregdmathews@gmail.com>
2013-03-12 15:45:12 -0700

    Adding zipcode webservice and amazon webservice

---
+++ src/webservice/WebService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservice;
+
+/**
+ *
+ * @author Greg
+ */
+public class WebService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
